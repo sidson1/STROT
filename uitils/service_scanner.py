09b61717c0f9a0f9e15dd820396f8fee9caa553d
anodicpassion <pratikpawarkarad@gmail.com>
2025-01-11 15:44:55 +0530

    utils updated

--- uitils/service_scanner.py
+++ uitils/service_scanner.py
@@ -1,1 +1,11 @@
 import nmap
+def service_scan(target_ip):
+    """
+    Runs an Nmap service scan on the given IP address.
+
+    Args:
+        target_ip (str): The IP address to scan.
+
+    Returns:
+        dict: Information about the services or an error message.
+    """

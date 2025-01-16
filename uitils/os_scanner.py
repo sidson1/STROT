3ea6988f53c6d51def3c8e56aacd13dac82945cb
anodicpassion <pratikpawarkarad@gmail.com>
2025-01-16 15:35:02 +0530

    newtork_scanner.py added to utils

--- uitils/os_scanner.py
+++ uitils/os_scanner.py
@@ -1,1 +1,13 @@
 import nmap
+
+
+def analyze_os(target_ip):
+    """
+    Analyzes the operating system of a given IP address using nmap.
+
+    Args:
+        target_ip (str): The IP address to analyze.
+
+    Returns:
+        dict: Information about the operating system or an error message.
+    """

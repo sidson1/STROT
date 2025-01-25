0b204fba5a1fd4d4334a92cbd6c712ffb4e6c12e
anodicpassion <pratikpawarkarad@gmail.com>
2025-01-25 15:28:49 +0530

    newtork_scanner.py added to utils

---
+++ uitils/network_scanner.py
@@ -0,0 +1,12 @@
+from scapy.all import ARP, Ether, srp
+
+def scan_network(ip_range):
+    """
+    Scans the network for active devices within the specified IP range.
+
+    Args:
+        ip_range (str): The IP range to scan, e.g., "192.168.1.1/24".
+
+    Returns:
+        list: A list of dictionaries containing IP and MAC addresses of active devices.
+    """
